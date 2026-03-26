92d6d5d6d1cb25c1b8c2aabb799b1da5484db2c6
Natalia García Cofré <nmt.garcia.c@gmail.com>
2026-03-26 15:30:54 -0300

    feat: mostrar catálogo de productos en frontend

--- store/urls.py
+++ store/urls.py
@@ -3,4 +3,5 @@ from . import views
 
 urlpatterns = [
     path("", views.home, name="home"),
+    path("productos/", views.product_list, name="product_list"),
 ]
--- store/views.py
+++ store/views.py
@@ -1,5 +1,11 @@
 from django.shortcuts import render
+from .models import Product
 
 
 def home(request):
     return render(request, "store/home.html")
+
+
+def product_list(request):
+    products = Product.objects.select_related("category").all().order_by("name")
+    return render(request, "store/product_list.html", {"products": products})
